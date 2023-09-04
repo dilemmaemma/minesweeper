2053fc8fd4e0623cbb5375e6430b7bcb827e1de2
delimmaemma <calvinhtml@gmail.com>
2023-09-04 17:16:19 -0500

    Added notes for TimeElapsed.js

---
+++ frontend/components/TimeElapsed.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+
+function TimeElapsed (playing) {
+    const [ time, setTime ] = useState(0)
+    const elapsedTime = () => {
+        const secondMarker = 1000
+        
+    }
+}
+
+export default function TimeElapsed;
