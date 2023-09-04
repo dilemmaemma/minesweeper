ec482321ff35ca042b2b4b295bb7a4988c24b6a1
delimmaemma <calvinhtml@gmail.com>
2023-09-04 16:35:32 -0500

    Fixed migration file

--- data/migrations/20230904182247_initial-migration.js
+++ data/migrations/20230904182247_initial-migration.js
@@ -1,39 +1,39 @@
 exports.up = async function(knex) {
-  await knex.schema
-    .createTable('difficulty', tbl => {
-        tbl.incrememnts('difficulty_id', tbl => {
-            tbl.string('difficulty', 6).notNullable()
-        })
-    })
-    .createTable('username', tbl => {
-        tbl.increments('username_id')
-        tbl.string('username', 15).notNullable()
-        tbl.string('ip_address', 128).notNullable()
-        tbl.integer('gamemode_id')
-            .unsigned()
-            .notNullable()
-            .references('gamemode_id')
-            .inTable('gamemode')
-            .onDelete('CASCADE')
-            .onUpdate('CASCADE')
-    })
-    .createTable('gamemode', tbl => {
-        tbl.incrememnts('gamemode_id')
-        tbl.real('time').notNullable()
-        tbl.integer('difficulty_id')
-            .unsigned()
-            .notNullable()
-            .references('difficulty_id')
-            .inTable('difficulty')
-            .onDelete('CASCADE')
-            .onUpdate('CASCADE')
-    })
-};
-
-exports.down = async function(knex) {
-  await knex.schema
-    .dropTableIfExists('gamemode')
-    .dropTableIfExists('username')
-    .dropTableIfExists('difficulty')
-
-};
+    await knex.schema
+      .createTable('difficulty', tbl => {
+          tbl.increments('difficulty_id')
+          tbl.string('difficulty', 12).notNullable()
+      })
+      .createTable('username', tbl => {
+          tbl.increments('username_id')
+          tbl.string('username', 15).notNullable()
+          tbl.string('ip_address', 128).notNullable()
+          tbl.integer('gamemode_id')
+              .unsigned()
+              .notNullable()
+              .references('gamemode_id')
+              .inTable('gamemode')
+              .onDelete('CASCADE')
+              .onUpdate('CASCADE')
+      })
+      .createTable('gamemode', tbl => {
+          tbl.increments('gamemode_id')
+          tbl.integer('time').notNullable()
+          tbl.integer('difficulty_id')
+              .unsigned()
+              .notNullable()
+              .references('difficulty_id')
+              .inTable('difficulty')
+              .onDelete('CASCADE')
+              .onUpdate('CASCADE')
+      })
+  };
+  
+  exports.down = async function(knex) {
+    await knex.schema
+      .dropTableIfExists('gamemode')
+      .dropTableIfExists('username')
+      .dropTableIfExists('difficulty')
+  
+  };
+  
