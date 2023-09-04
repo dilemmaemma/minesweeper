c22629d1c3bc8fb8425d2de0aff76e41ba5c2e95
delimmaemma <calvinhtml@gmail.com>
2023-09-04 14:20:23 -0500

    Started creation of highscore database

--- data/migrations/20230904182247_initial-migration.js
+++ data/migrations/20230904182247_initial-migration.js
@@ -1,5 +1,11 @@
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('username', tbl => {
+        tbl.increments('username_id')
+        tbl.string('username', 15).notNullable()
+        tbl.string('ip_address', 32).notNullable()
+        tbl.integer([FOREIGN KEY HERE])
+    })
 };
 
 exports.down = function(knex) {
