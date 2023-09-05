b20cdd8b9238d30d75fb252bc7051754a7d89d36
delimmaemma <calvinhtml@gmail.com>
2023-09-04 20:28:25 -0500

    Added placeholders in TimeElapsed.js for testing

--- frontend/components/TimeElapsed.js
+++ frontend/components/TimeElapsed.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react'
 
 function TimeElapsed (playing) {
+    playing = true // Placeholder for testing
+
     const [ time, setTime ] = useState(0)
 
     useEffect(() => {
         // Function to update the elapsed seconds
         const updateElapsedSeconds = () => {
           setTime((prevElapsedSeconds) => prevElapsedSeconds + 1);
+          if (time === 10) playing = false // Placeholder for testing
+          if (!playing) {
+            clearInterval(intervalId)
+            return
+          }
         };
     
         // Set up an interval to update the elapsed seconds every 1000 milliseconds (1 second)
