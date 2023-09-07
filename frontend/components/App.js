c48696258600ab0602ea660664de232fbd2a7209
delimmaemma <calvinhtml@gmail.com>
2023-09-06 19:11:18 -0500

    Fixed issue with board render and Gamemode link

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -39,9 +39,9 @@ function App() {
             <ul className="menu__box">
               <nav>
                 <li><Link to='/' className='menu__item'>Home</Link></li>
-                <li><p className='menu__item' onClick={() => toggleLinks()} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} style={{ cursor: isMouseOver ? 'pointer' : 'default' }}>Gamemodes {!showLinks ? '▼' : '▲'}</p></li>
+                <li><Link to='/' className='menu__item' onClick={() => toggleLinks()} >Gamemodes {!showLinks ? '▼' : '▲'}</Link></li>
                 <li><Link to='easy' className={`special menu__item ${!showLinks ? 'hidden' : ''}`}>Easy</Link></li>
-                <li><Link to='medium' className={`special menu__item ${!showLinks ? 'hidden' : ''}`}>Intermediate</Link></li>
+                <li><Link to='intermediate' className={`special menu__item ${!showLinks ? 'hidden' : ''}`}>Intermediate</Link></li>
                 <li><Link to='expert' className={`special menu__item ${!showLinks ? 'hidden' : ''}`}>Expert</Link></li>
                 <li><Link to='custom' className={`special menu__item ${!showLinks ? 'hidden' : ''}`}>Custom</Link></li>
                 <li><Link to='highscores' className='menu__item'>High Scores</Link></li>
@@ -56,8 +56,8 @@ function App() {
         <Route path='highscores' element={<Highscores />} />
         <Route path='about' element={<About />} />
         <Route path='easy' element = {<Board difficulty='easy' />} />
-        <Route path='medium' element = {<Board difficulty='medium' />} />
-        <Route path='hard' element = {<Board difficulty='hard' />} />
+        <Route path='intermediate' element = {<Board difficulty='medium' />} />
+        <Route path='expert' element = {<Board difficulty='hard' />} />
         <Route path='custom' element = {<Board difficulty='custom' />} />
     </Routes>
   </>
--- frontend/components/boards/Board.js
+++ frontend/components/boards/Board.js
@@ -1,11 +1,96 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+// import axios from 'axios'
 
-function Board () {
+import CustomBoard from './CustomBoard'
+
+let board
+
+function Board ({difficulty}) {
+
+    const [game, setGame] = useState([])
+
+    useEffect(() => {
+        if (difficulty === 'easy') {
+            board = {
+                bombs: 10,
+                width: 8,
+                height: 8,
+            }
+        } else if (difficulty === 'medium') {
+            board = {
+                bombs: 40,
+                width: 16,
+                height: 16,
+            }
+        } else if (difficulty === 'hard') {
+            board = {
+                bombs: 99,
+                width: 30,
+                height: 16,
+            }
+        } else if (difficulty === 'custom') {
+            <CustomBoard/>
+            // axios get from custom board api
+        } else {
+            board = {
+                bombs: 8,
+                width: 8,
+                height: 10,
+            }
+        }
+        if (game.length < 8 || !game) {
+            let bombPlacement = createGameBoard(board)
+            setGame(bombPlacement)
+        } else return
+    }, [difficulty]);
+
+    function generateRandomBombPositions(board) {
+        const bombPositions = [];
+      
+        while (bombPositions.length < board.bombs) {
+          const x = Math.floor(Math.random() * board.width);
+          const y = Math.floor(Math.random() * board.height);
+          const position = `${x}-${y}`;
+      
+          // Ensure there are no duplicates
+          if (!bombPositions.includes(position)) {
+            bombPositions.push(position);
+          }
+        }
+      
+        return bombPositions;
+    }
+      
+    function createGameBoard(board) {
+        const bombPositions = generateRandomBombPositions(board);
+        const newGame = [];
+      
+        for (let i = 0; i < board.height; i++) {
+          const row = [];
+      
+          for (let j = 0; j < board.width; j++) {
+            const isBomb = bombPositions.includes(`${j}-${i}`);
+            row.push(isBomb ? 'X' : 'O');
+          }
+      
+          newGame.push(row);
+        }
+      
+        return newGame;
+    }
+
+    console.log(game)
+
+    function newBoard() {
+        setGame([])
+    }
+
     return (
         <div>
-            <h1>Test</h1>
+            
         </div>
     )
 }
 
 export default Board
+
