8ce0dc51c309bb85e08dbb369c997cb3d50c9af7
delimmaemma <calvinhtml@gmail.com>
2023-09-06 21:18:53 -0500

    Fixed board logic

--- frontend/components/boards/Board.js
+++ frontend/components/boards/Board.js
@@ -1,55 +1,57 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 // import axios from 'axios'
 
-import CustomBoard from './CustomBoard'
+import CustomBoard from './CustomBoard';
 
-import '../../css/home.css'
+let board
 
-function Board ({difficulty}) {
-    const [game, setGame] = useState([])
-    const [level, setLevel] = useState(difficulty)
+function Board({ difficulty }) {
+  const [game, setGame] = useState([]);
+  const [level, setLevel] = useState(difficulty); //eslint-disable-line
 
-    if (!localStorage.getItem('difficulty')) localStorage.setItem('difficulty', difficulty)
-    if (localStorage.getItem('difficulty') !== difficulty) localStorage.setItem('difficulty', difficulty)
+  // Load board data from localStorage when component mounts
+  useEffect(() => {
+    if (localStorage.getItem('board')) {
+      setGame(JSON.parse(localStorage.getItem('board')));
+    }
+    createBoard()
+  }, []);
 
-    useEffect(() => {
-        let board
-        // Keeps difficulty the same if move away from current page
-        if (localStorage.getItem('difficulty')) difficulty = localStorage.getItem('difficulty')
+  // Function to create a new board based on the given level or difficulty
+  function createBoard() {
+    if (level === 'easy') {
+      const board = {
+        bombs: 10,
+        width: 8,
+        height: 8,
+      };
+      const bombPlacement = createGameBoard(board);
+      setGame(bombPlacement);
+      localStorage.setItem('board', JSON.stringify(board));
+    } else if (level === 'medium') {
+        board = {
+            bombs: 40,
+            width: 16,
+            height: 16,
+        };
+        const bombPlacement = createGameBoard(board);
+        setGame(bombPlacement);
+        localStorage.setItem('board', JSON.stringify(board));
+    } else if (level === 'custom') {
+        board = {
+            bombs: 99,
+            width: 30,
+            height: 16,
+        }
+        const bombPlacement = createGameBoard(board);
+        setGame(bombPlacement);
+        localStorage.setItem('board', JSON.stringify(board));
+    }
+  }
 
-        // To prevent errors, state is only set if difficulty is not equal to custom
-        // If it is custom difficulty, useEffect is ignored and the component is rendered in the DOM
-
-        if (level === 'easy') {
-            board = {
-                bombs: 10,
-                width: 8,
-                height: 8,
-            }
-        } else if (level === 'medium') {
-            board = {
-                bombs: 40,
-                width: 16,
-                height: 16,
-            }
-        } else if (level === 'hard') {
-            board = {
-                bombs: 99,
-                width: 30,
-                height: 16,
-            }
-        } else if (level === 'custom') {
-            // axios call here
-        } 
-        if (level !== 'custom') {
-            let bombPlacement = createGameBoard(board)
-            setGame(bombPlacement)
-            if (localStorage.getItem('board')) setGame(localStorage.getItem('board'))
-        } else return
-    }, [level]);
-
-    function generateRandomBombPositions(board) {
-        const bombPositions = [];
+  // Function to generate bomb positions for a given board
+  function generateRandomBombPositions(board) {
+    const bombPositions = [];
       
         while (bombPositions.length < board.bombs) {
           const x = Math.floor(Math.random() * board.width);
@@ -63,10 +65,11 @@ function Board ({difficulty}) {
         }
       
         return bombPositions;
-    }
-      
-    function createGameBoard(board) {
-        const bombPositions = generateRandomBombPositions(board);
+  }
+
+  // Function to create a game board based on a board configuration
+  function createGameBoard(board) {
+    const bombPositions = generateRandomBombPositions(board);
         const newGame = [];
       
         for (let i = 0; i < board.height; i++) {
@@ -81,25 +84,24 @@ function Board ({difficulty}) {
         }
       
         return newGame;
-    }
+  }
 
-    console.log(game)
+  // Function to start a new board
+  function newBoard() {
+    setGame([]);
+    localStorage.removeItem('board');
+    createBoard();
+  }
 
-    function newBoard() {
-        setGame([])
-        localStorage.removeItem('board')
-        setLevel(level)
-    }
+  console.log(game)
 
-    return (
-        <div className='placeholder'>
-            {localStorage.setItem('board', game)}
-            <br/><br/><br/>
-            {level === 'custom' && <CustomBoard/>}
-            <button onClick={newBoard}>Get New Board</button>
-        </div>
-    )
+  return (
+    <div className='placeholder'>
+      <br/><br/><br/>
+      {level === 'custom' && <CustomBoard />}
+      <button onClick={newBoard}>Get New Board</button>
+    </div>
+  );
 }
 
-export default Board
-
+export default Board;
