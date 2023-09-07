5bdc386f73a12bffa65b75ba97649dac0a1c6ac2
delimmaemma <calvinhtml@gmail.com>
2023-09-06 22:13:27 -0500

    Added navigation when component unmounts

--- frontend/components/boards/Board.js
+++ frontend/components/boards/Board.js
@@ -1,114 +1,5 @@
-// import React, { useState, useEffect } from 'react';
-// // import axios from 'axios'
-
-// import CustomBoard from './CustomBoard';
-
-// let board
-
-// function Board({ difficulty }) {
-//   const [game, setGame] = useState([]);
-//   const [level, setLevel] = useState(difficulty); //eslint-disable-line
-
-//   // Load board data from localStorage when component mounts
-//   useEffect(() => {
-//     if (localStorage.getItem('board')) {
-//       setGame(JSON.parse(localStorage.getItem('board')));
-//     }
-//         const newGame = createBoard()
-//     setGame(newGame)
-
-//     localStorage.setItem('board', JSON.stringify(game));
-//    }, []);
-
-//   // Function to create a new board based on the given level or difficulty
-//   function createBoard() {
-//     if (level === 'easy') {
-//       const board = {
-//         bombs: 10,
-//         width: 8,
-//         height: 8,
-//       };
-//         const bombPlacement = createGameBoard(board);
-//         return bombPlacement;
-//     } else if (level === 'medium') {
-//       board = {
-//         bombs: 40,
-//         width: 16,
-//         height: 16,
-//       };
-//         const bombPlacement = createGameBoard(board);
-//         return bombPlacement;
-//     } else if (level === 'custom') {
-//       board = {
-//         bombs: 99,
-//         width: 30,
-//         height: 16,
-//       };
-//       const bombPlacement = createGameBoard(board);
-//       return bombPlacement;
-//     }
-//   }
-  
-//   // Function to generate bomb positions for a given board
-//   function generateRandomBombPositions(board) {
-//     const bombPositions = [];
-      
-//         while (bombPositions.length < board.bombs) {
-//           const x = Math.floor(Math.random() * board.width);
-//           const y = Math.floor(Math.random() * board.height);
-//           const position = `${x}-${y}`;
-      
-//           // Ensure there are no duplicates
-//           if (!bombPositions.includes(position)) {
-//             bombPositions.push(position);
-//           }
-//         }
-      
-//         return bombPositions;
-//   }
-
-//   // Function to create a game board based on a board configuration
-//   function createGameBoard(board) {
-//     const bombPositions = generateRandomBombPositions(board);
-//         const newGame = [];
-      
-//         for (let i = 0; i < board.height; i++) {
-//           const row = [];
-      
-//           for (let j = 0; j < board.width; j++) {
-//             const isBomb = bombPositions.includes(`${j}-${i}`);
-//             row.push(isBomb ? 'X' : 'O');
-//           }
-      
-//           newGame.push(row);
-//         }
-      
-//         return newGame;
-//   }
-
-//   // Function to start a new board
-//   function newBoard() {
-//     setGame([]);
-//     localStorage.removeItem('board');
-//     const newGame = createBoard();
-//     setGame(newGame);
-//     localStorage.setItem('board', JSON.stringify(game));
-//   }
-
-//   console.log(game)
-
-//   return (
-//     <div className='placeholder'>
-//       <br/><br/><br/>
-//       {level === 'custom' && <CustomBoard />}
-//       <button onClick={newBoard}>Get New Board</button>
-//     </div>
-//   );
-// }
-
-// export default Board;
-
 import React, {useState, useEffect} from 'react'
+import { Navigate } from 'react-router'
 // import axios from 'axios'
 
 import CustomBoard from './CustomBoard'
@@ -154,6 +45,9 @@ function Board ({difficulty}) {
 
         let bombPlacement = createGameBoard(board)
         setGame(bombPlacement)
+
+        // Navigate to home page when component unmounts
+        return () => {<Navigate to='/'/>}
     }, [difficulty]);
 
     localStorage.setItem('board', JSON.stringify(game))
@@ -234,7 +128,6 @@ function Board ({difficulty}) {
 
     return (
         <div className='placeholder'>
-            {console.log(level)}
             <br/><br/><br/>
             {level === 'custom' && <CustomBoard />}
             {level !== 'custom' && <button onClick={newBoard}>Get New Board</button>}
