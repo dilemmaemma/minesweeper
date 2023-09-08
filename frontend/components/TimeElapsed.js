93f512ac640b7ff41e846a3c457d30dff4217201
delimmaemma <calvinhtml@gmail.com>
2023-09-07 19:45:21 -0500

    Fixed bug in TimeElapsed.js

--- frontend/components/TimeElapsed.js
+++ frontend/components/TimeElapsed.js
@@ -26,7 +26,7 @@ function TimeElapsed (playing) {
       }, []);
 
     return (
-        <h1 {{color: 'white'}}>Time elapsed: {time}</h1>
+        <h1 style={{color: 'white'}}>Time elapsed: {time}</h1>
     )
 }
 
