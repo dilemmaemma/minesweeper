89ea28fc9a63bc28d63b5808676341d816714a5a
delimmaemma <calvinhtml@gmail.com>
2023-09-12 15:52:28 -0500

    Moved folders around & started on api development

--- backend/api/server.js
+++ backend/api/server.js
@@ -2,10 +2,17 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 
+const customBoardRouter = require('./customBoard/router')
+const highscoreRouter = require('./highscore/router')
+
 const server = express()
 
 server.use(express.json())
 server.use(cors())
+
+server.get('/api/customboard', customBoardRouter)
+
+server.get('/api/highscore', highscoreRouter)
 
 server.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'))
